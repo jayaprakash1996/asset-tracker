aa35152050e9ba2bb8c7ca76438a6dc65913edfc
jayaprakash1996 <90298693+jayaprakash1996@users.noreply.github.com>
2026-04-27 23:29:27 +0530

    minimal change

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
-    reducer:{
-        // all reducers will be placed here
-        auth: authReducer,
-    },
+  reducer: {
+    auth: authReducer,
+  },
 });
